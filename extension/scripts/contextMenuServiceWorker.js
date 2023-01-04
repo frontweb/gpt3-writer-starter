db37177ba30480458039eb2b48c7deea5988e090
Radu Cojocaru <rcojocaru@gmail.com>
2023-01-04 14:13:15 +0200

    Add service worker

---
+++ extension/scripts/contextMenuServiceWorker.js
@@ -0,0 +1,63 @@
+const getKey = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(["openai-key"], (result) => {
+      if (result["openai-key"]) {
+        const decodedKey = atob(result["openai-key"]);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+
+const generate = async (prompt) => {
+  // Get your API key from storage
+  const key = await getKey();
+  const url = "https://api.openai.com/v1/completions";
+
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 450,
+      temperature: 0.7,
+    }),
+  });
+
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+};
+
+const generateCompletionAction = async (info) => {
+  try {
+    const { selectionText } = info;
+    const prompt = `Ask Dave Ramsey if it's ok or not to purchase the following item: ${selectionText}
+And to explain why
+Dave's response:\n`;
+
+    // Add this to call GPT-3
+    const baseCompletion = await generate(prompt);
+
+    // Let's see what we get!
+    console.log(baseCompletion.text);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "context-run",
+    title: "Get Dave Ramsey advice on purchase",
+    contexts: ["selection"],
+  });
+});
+
+// Add listener
+chrome.contextMenus.onClicked.addListener(generateCompletionAction);
